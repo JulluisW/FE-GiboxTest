b08f87063bf0db02517679c5d075dc2355dd98d2
Julluis Wiputra <julluisw@gmail.com>
2022-06-15 21:39:15 +0700

    Routes (#3)
    * Set Up Routes with React Router DOM
    
    * Setting up Routes for the web

---
+++ fe-gibox-test/src/ProtectedRoutes.js
@@ -0,0 +1,12 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const useAuth = () => {
+  const user = { loggedIn: false };
+  return user && user.loggedIn;
+};
+
+export function ProtectedRoutes() {
+  const isAuth = useAuth()
+
+  return isAuth ? <Outlet /> : <Navigate to="/login" />
+}
