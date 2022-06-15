1ba03ebe8f02f7aedff675a0d1d25ec8bc5ac5aa
JulluisW <julluisw@gmail.com>
2022-06-15 20:08:45 +0700

    Set Up Routes with React Router DOM

---
+++ fe-gibox-test/src/pages/Students.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Students() {
+  return (
+    <div>students</div>
+  )
+}
---
+++ fe-gibox-test/src/pages/index.js
@@ -0,0 +1,5 @@
+export {Books} from './Books.js';
+export {Login} from './Login.js';
+export {Rent} from './Rent.js';
+export {Students} from './Students.js';
+export {Notfound} from './Notfound.js';
