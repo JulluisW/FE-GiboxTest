6a98bf505cc2d1e971f75c6887a06ec7511c0de5
JulluisW <julluisw@gmail.com>
2022-06-16 03:40:11 +0700

    Create Not Found Page

--- fe-gibox-test/src/pages/not-found/Notfound.js
+++ fe-gibox-test/src/pages/not-found/Notfound.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import "./style.scss";
 
 export function Notfound() {
   return (
-    <div>Notfound</div>
-  )
+    <div className="not-found-page">
+      <img src="https://i0.wp.com/saedx.com/blog/wp-content/uploads/2019/01/saedx-blog-featured-70.jpg?fit=1200%2C500&ssl=1" alt="notfound" />
+      <Button size="large">
+        <Link to="/books">Go Back</Link>
+      </Button>
+    </div>
+  );
 }
